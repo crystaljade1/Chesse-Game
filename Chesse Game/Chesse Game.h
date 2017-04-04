49049829ae556be30c7ccb524bd147c1d5d8e0cc
Crystal Jade Allen-Washington <crystaljade@Macbooks-MacBook-Air.local>
2017-04-04 13:31:48 -0400

    Initial Commit

---
+++ Chesse Game/Chesse Game.h
@@ -0,0 +1,19 @@
+//
+//  Chesse Game.h
+//  Chesse Game
+//
+//  Created by Crystal Jade Allen-Washington on 4/4/17.
+//  Copyright © 2017 Crystal Jade Allen-Washington. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Chesse Game.
+FOUNDATION_EXPORT double Chesse_GameVersionNumber;
+
+//! Project version string for Chesse Game.
+FOUNDATION_EXPORT const unsigned char Chesse_GameVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Chesse_Game/PublicHeader.h>
+
+
